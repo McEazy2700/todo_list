406fce1de18257ccfce87037ed80322093d6357a
Ezekiel Victor <codeepoch@gmail.com>
2022-05-08 19:17:18 +0100

    Added login, register, and search functionality

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 
 urlpatterns = [
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', views.TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task_detail'),
     path('create-task/', views.TaskCreate.as_view(), name='create-item'),
